d33c64d254445b57076b2ec369dcbca2b6b9748c
dahdouhMouad <mouaddhd10@gmail.com>
2023-11-27 19:17:42 +0100

    Initial Commit

---
+++ SaveLinks/SaveLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveLinksApp.swift
+//  SaveLinks
+//
+//  Created by Macbook Pro on 27/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SaveLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
